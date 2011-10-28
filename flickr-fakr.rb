660630407c8991b4c63c368f5c0003134b4f147c
James Ottaway <james@ottaway.mp>
2011-10-28 18:07:05 +1000

    Created a fake REST endpoint which renders the HAML view based off the method and format args

--- flickr-fakr.rb
+++ flickr-fakr.rb
@@ -7,3 +7,11 @@ end
 get '/services/auth/' do
   redirect 'http://localhost:9292/flickr/callback?frob=123'
 end
+
+post '/services/rest/' do
+  args = {}
+  request.body.rewind
+  request.body.read.split('&').each { |arg| args[arg.split('=').first] = arg.split('=').last }
+  view = "#{args['method']}.#{args['format']}"
+  haml view.to_sym
+end
